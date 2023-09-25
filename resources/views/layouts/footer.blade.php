f880001da716f305be2689831270d9c2fb15bdca
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-09-25 20:10:54 +0700

    h

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,10 @@
-<x-app-layout>
+@extends('layout.app')
+@section('footer')
     <footer class="main-footer">
         <strong>Copyright &copy; 2023 <a href="#">Iklan</a>.</strong>
         All rights reserved.
         <!-- <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
-    </div> -->
+            <b>Version</b> 1.0.0
+        </div> -->
     </footer>
-</x-app-layout>
+@endsection
