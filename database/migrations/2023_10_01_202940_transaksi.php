e6b3351f06dec88cfb1f9a71fe06bef2d51a1464
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-10-02 03:54:15 +0700

    df

---
+++ database/migrations/2023_10_01_202940_transaksi.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->double('nominal');
+            $table->text('keterangan')->nullable();
+            $table->date('tanggal');
+            $table->tinyInteger('jenis_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
